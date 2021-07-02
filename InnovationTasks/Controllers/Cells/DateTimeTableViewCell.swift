9ef83dcfcfc8bbc8cdc97d12b933e5a77102eb9b
Renato Ruiz <renatoruiz2607@gmail.com>
2021-07-02 13:34:22 -0300

    Implemented Create Task Screen, using DatePicker to set the dateCell, already with a toolBar with a done Button to place the date on the textField (Date already formatted)

---
+++ InnovationTasks/Controllers/Cells/DateTimeTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateTimeTableViewCell.swift
+//  InnovationTasks
+//
+//  Created by Roberto Ruiz Cai on 02/07/21.
+//  Copyright © 2021 Renato Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class DateTimeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateTimeTextField: UITextField!
+    
+}
---
+++ InnovationTasks/Controllers/Cells/TaskDescriptionTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TaskDescriptionTableViewCell.swift
+//  InnovationTasks
+//
+//  Created by Roberto Ruiz Cai on 02/07/21.
+//  Copyright © 2021 Renato Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class TaskDescriptionTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var taskDescriptionTextField: UITextField!
+    
+}
